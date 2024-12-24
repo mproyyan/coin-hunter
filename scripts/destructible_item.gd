9c32b1af32cde0cffcc718c2a4a4c92c51a9effd
Muhammad Royyan <mproyyan@gmail.com>
2024-12-24 11:10:37 +0700

    Connect destructible item with script

--- scripts/destructible_item.gd
+++ scripts/destructible_item.gd
@@ -1,6 +1,19 @@
 extends AnimatableBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var hitbox: Area2D = $Hitbox
+
+func _physics_process(delta: float) -> void:
+	if check_if_player_body_already_in_hitbox() and Global.player_current_rollin:
+		animation_player.play("destroyed")
+
+func check_if_player_body_already_in_hitbox() -> bool:
+	var overlapping_bodies = hitbox.get_overlapping_bodies()
+	for overlap_body in overlapping_bodies:
+		if overlap_body.has_method("player"):
+			return true
+	
+	return false
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	if body.has_method("player") and Global.player_current_rollin:
