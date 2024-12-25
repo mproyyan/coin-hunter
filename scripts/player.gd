714058f1a179fc52418c40f88153207b085e0465
Muhammad Royyan <mproyyan@gmail.com>
2024-12-25 08:58:13 +0700

    Create function to change direction state

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var jump_audio: AudioStreamPlayer2D = $JumpAudio
+
+var direction_state = 1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,13 +16,23 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_audio.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	change_direction_state(direction)
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func change_direction_state(direction: int):
+	if direction == 1:
+		direction_state = 1
+	elif direction == -1:
+		direction_state = -1
