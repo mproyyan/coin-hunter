1d701e61f4e87de7a34aa12997ec32b306e415e2
Muhammad Royyan <mproyyan@gmail.com>
2024-12-25 09:20:28 +0700

    Stop rollin

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,9 +3,10 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-const ROLL_SPEED = 300
+const ROLL_SPEED = 400
 
 @onready var jump_audio: AudioStreamPlayer2D = $JumpAudio
+@onready var rollin_timer: Timer = $RollinTimer
 
 var direction_state = 1
 var rollin_in_progress = false
@@ -28,11 +29,11 @@ func _physics_process(delta: float) -> void:
 	
 	rollin()
 	if rollin_in_progress:
-		print("ok", ROLL_SPEED * direction_state)
+		print(ROLL_SPEED * direction_state)
 		velocity.x = ROLL_SPEED * direction_state
 	
 	if direction:
-		velocity.x = direction * ROLL_SPEED
+		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
@@ -49,3 +50,9 @@ func rollin():
 		print("user press rollin")
 		Global.player_current_rollin = true
 		rollin_in_progress = true
+		rollin_timer.start()
+
+func _on_rollin_timer_timeout() -> void:
+	rollin_timer.stop()
+	Global.player_current_rollin = false
+	rollin_in_progress = false
