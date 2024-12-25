ced5e75c152ea1e4e60cb89b94d96c621ea7996a
Muhammad Royyan <mproyyan@gmail.com>
2024-12-25 11:56:18 +0700

    Place more coins

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,5 +3,6 @@ extends Area2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
-	animation_player.play("pickup")
+	if body.has_method("player"):
+		animation_player.play("pickup")
 	
--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -1,8 +1,8 @@
 extends CanvasLayer
 
 func _on_play_again_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 	Global.player_alive = true
 	Global.player_health = 100
 	Global.player_current_rollin = false
 	Global.can_play_frostbite_animation = false
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
